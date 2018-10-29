c54e2f52d265750bb49c27a11a286c5a9a4a3d56
[Leanne] <mcnicholl-l@email.ulster.ac.uk>
2018-10-29 15:06:57 +0000

    creating login/ register page

--- app/src/main/java/com/example/leann/bookingsystem/RegisterPage.java
+++ app/src/main/java/com/example/leann/bookingsystem/RegisterPage.java
@@ -1,18 +1,24 @@
 package com.example.leann.bookingsystem;
-
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
-public class ContactUsPage extends Fragment {
+public class RegisterPage extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.contact_us_page, container, false);
+        return inflater.inflate(R.layout.register_page, container, false);
+
+
     }
 
-}
+
+   }
+
+
